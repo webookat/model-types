5d71d98671e0eef2eb9f958adab85dc3259fab40
nikoloza <nika.tomadze@gmail.com>
2019-06-24 18:38:31 +0400

    Updates

---
+++ precache-manifest.d9c311667540b2f2b07c8843c45c8a86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e873b8d405365ba3a3b79f6e29e24de",
+    "url": "/model-types/index.html"
+  },
+  {
+    "revision": "b6898d6af0d7cf816160",
+    "url": "/model-types/static/css/main.d1f1d9b8.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/model-types/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "b6898d6af0d7cf816160",
+    "url": "/model-types/static/js/main.fb49f632.chunk.js"
+  },
+  {
+    "revision": "de16c0beb508695090c9",
+    "url": "/model-types/static/js/runtime~main.c10af7ce.js"
+  }
+]);
